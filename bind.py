b89b2340d63dc6ebbf156a92f868728c4e7b2555
marcelo_do_ogbon <marcelo@ogbon>
2022-08-01 20:19:08 -0300

    More simplifications, but error still persists

--- bind.py
+++ bind.py
@@ -1,7 +1,6 @@
-from ctypes import *
+
 import ctypes
-import numpy as np
-#import torch
+import torch
 
 so_file = "./mylib.so"
 
@@ -10,5 +9,6 @@ my_functions = ctypes.CDLL(so_file)
 my_functions.addvector_cab.restype = ctypes.c_int
 
 if( my_functions.addvector_cab() == 0):
-    print("Returned OKAY!")
+     print("Returned OKAY!")
+
 
